7ae5adfc105dea05ff39185e548a77405b4353b0
damianwick <110914265+damianwick@users.noreply.github.com>
2023-01-25 19:55:45 +0000

    Connect contact form to formspree.

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,6 +1,32 @@
+
+import { useForm, ValidationError } from '@formspree/react'
 import './ContactSection.css'
 
 export default function ContactSection() {
+    const [state, handleSubmit] = useForm("xrgvezda");
+    if (state.succeeded) {
+        return (
+            <section id="contact-section">
+             <div className='background-signs'>
+                <h1>CONTACT</h1>
+                <h1>CONTACT</h1>
+            </div>
+           <div className='contact-container'>
+                <div className='contact-label'>
+                    <p>
+                    Would you like us to work together? Please leave a message using the form below or contact me directly
+                    at <a href='mailto:damianr.wick@gmail.com'><strong>damianr.wick@gmail.com</strong></a>.
+                    </p>
+                </div>
+                <div className='sent-message-container'>
+                    <h2>Thank you for your message!</h2>
+                </div>
+
+            </div> 
+        </section>
+    )
+    }
+
     return (
         <section id="contact-section">
              <div className='background-signs'>
@@ -14,7 +40,7 @@ export default function ContactSection() {
                     at <a href='mailto:damianr.wick@gmail.com'><strong>damianr.wick@gmail.com</strong></a>.
                     </p>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className='contact-form-first-row'>
                         <div>
                             <label htmlFor="name">Name</label>
@@ -22,14 +48,26 @@ export default function ContactSection() {
                         </div>
                         <div>
                             <label htmlFor="email">Email</label>
-                            <input type='text' name='email' id='email'/>
+                            <input type='email' name='email' id='email'/>
+                            <ValidationError 
+                                prefix="Email" 
+                                field="email"
+                                errors={state.errors}
+                            />
                         </div>
                     </div>  
                     <label htmlFor="subject">Subject</label>
                     <input type='text' name='subject' id='subject'/>
                     <label htmlFor="message">Message</label>
                     <textarea type='text' name='message' id='message'/>
-                    <input type='submit' value="Submit" className='btn form-btn'/>
+                    <ValidationError 
+                        prefix="Message" 
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <button type='submit' disabled={state.submitting} className='btn form-btn'>
+                        Submit
+                    </button>
                 </form>
             </div> 
         </section>
